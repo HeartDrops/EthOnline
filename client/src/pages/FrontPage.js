c5c2ea712aeec4fdd4982f81c96c76f5ce1ad7cf
Iris <iris.devillars@gmail.com>
2021-09-30 21:28:59 +0200

    Update FrontPage

--- client/src/pages/FrontPage.js
+++ client/src/pages/FrontPage.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FrontPage = () => {
-  return <div>LANDING PAGE</div>;
+  return (
+    <>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="flex-col hero-content lg:flex-row-reverse">
+        <img src="https://picsum.photos/id/1005/600/600" className="max-w-sm rounded-lg shadow-2xl" /> 
+        <div>
+          <h1 className="mb-5 text-5xl font-bold">
+            Heart Drops
+          </h1> 
+          <p className="mb-5">
+            An NFT fractionalization platform that allows an art community to donate art, share art ownership, and raise funds for charities.
+          </p>
+          <Link  to="/auctions" className="btn btn-primary mr-2">Discover Heart Drops</Link>
+          <Link  to="/create" className="btn btn-secondary">Create an Heart Drops</Link>
+        </div>
+      </div>
+    </div>
+    <div className="">
+      <h2 className="m-5 text-5xl font-bold text-center">Current Heart Drops</h2>
+    </div>
+    </>
+  );
 };
 
 export default FrontPage;
