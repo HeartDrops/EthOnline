a3f3a55e648d5a42830c4fa39d126c913c3bb0f2
Dinesh <dineshse1986@gmail.com>
2021-10-03 23:40:18 -0400

    Erc721 add.

--- contracts/ACHouse.sol
+++ contracts/ACHouse.sol
@@ -1,16 +1,19 @@
 // "SPDX-License-Identifier: UNLICENSED"
-
 pragma solidity ^0.8.3;
 
 import "./ACHouseToken1155.sol";
+import "./ACHouseToken721.sol";
 import "./ACHouseToken20.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ACHouse  {
 
     address owner;
 
-    ACHouseToken20 token;
-    IERC1155 multitoken;
+    ACHouseToken1155 multitoken;
+    ACHouseToken721 nftToken;
+    ACHouseToken20 fractoken;
 
     // constructor(address _erc1155token){
 
@@ -20,36 +23,62 @@ contract ACHouse  {
     // }
     
     /** Variables and Event Emitters */
-    struct Auction {
+    struct AuctionPlace {
+        uint256 id;
         string name;
-        address id; // wallet address of init setup
     }
     struct Artist {
+        uint256 id;
         string name;
-        address id; // wallet address of init setup
     }
     struct Participant {
+        uint256 id;
         string name;
-        address id;
     }
-    
-    mapping (address => Auction) AuctionMapping;
+    struct NGO {
+        uint256 id;
+        string name;
+    }
+
+    uint auctionPlaceCount = 0;
+    uint artistCount = 0;
+    uint participantCount = 0;
+    uint ngoCount = 0;
+    NGO[] public ngoArray;
+    address[] RegisteredUserAddresses; // might be used??
+
+    // mapping address to type
+    mapping (address => AuctionPlace) AuctionPlaceMapping;
     mapping (address => Artist) ArtistMapping;
     mapping (address => Participant ) ParticipantMapping;
+    mapping (address => NGO ) NgoMapping; 
     
-    address[] RegisteredUserAddresses;
-    
+    // User address maps to - Chaities and the donation amount. 
+    mapping ( address =>  mapping (address => uint256)) UserNGODonation; 
+
+    struct NFTContracts{
+        ACHouseToken721 contracts;
+        address nftcontractAddress;
+    }
+
     struct fracNFTContracts{
         ACHouseToken20 contracts;
-        address contractAddress;
+        address fraccontractAddress;
     }
+    mapping (string => NFTContracts) NFTContractsMappings;
     mapping (string => fracNFTContracts) fracNFTContractsMappings;
     
     
     /** Getters function */
+    function getNftData(string memory _nftName) public view returns (NFTContracts memory){
+        return NFTContractsMappings[_nftName];
+    }
+    
     function getfracNftData(string memory _shard) public view returns (fracNFTContracts memory){
         return fracNFTContractsMappings[_shard];
     }
+
+
     
     /**Functions - Registration  */
     function register(string memory _name, uint _userType) public returns (bool) {
@@ -58,27 +87,37 @@ contract ACHouse  {
         //userType { 1: Auction, 2: Artist, 3: Participant}
         // register user by type
         if(_userType == 1){
-            Auction memory ac = Auction(_name, msg.sender);
-            AuctionMapping[msg.sender] = ac;
+            AuctionPlace memory ac = AuctionPlace(auctionPlaceCount, _name);
+            auctionPlaceCount++;
+
+            AuctionPlaceMapping[msg.sender] = ac;
         }else if( _userType == 2){
-            Artist memory ar = Artist(_name, msg.sender);
+            Artist memory ar = Artist(artistCount, _name);
+            artistCount++;
+
             ArtistMapping[msg.sender] = ar;
         }else {
-            Participant memory part = Participant(_name, msg.sender);
+            Participant memory part = Participant(participantCount, _name);
+            participantCount++;
+
             ParticipantMapping[msg.sender] = part;
         }
 
         RegisteredUserAddresses.push(msg.sender);
         return true;
     }
-    
+
+    function createNFt() public {
+
+    }
+
     function fracNFT(string memory _shardName, string memory _shardSymbol, uint256 supply) public {
         // owner retain - amount of shards (tokes) will be given to the user, if all then end of steps. 
         // if user wants to generate funds, owner retain will be less than 100 and user sets price per shard (token). 
         
-        token = new ACHouseToken20(_shardName, _shardSymbol, supply);
+        fractoken = new ACHouseToken20(_shardName, _shardSymbol, supply);
         
-        fracNFTContracts memory cont = fracNFTContracts(token, address(token));
+        fracNFTContracts memory cont = fracNFTContracts(fractoken, address(fractoken));
         
         fracNFTContractsMappings[_shardName] = cont;
     }
--- contracts/ACHouseToken721.sol
+++ contracts/ACHouseToken721.sol
@@ -1,2 +1,17 @@
 pragma solidity ^0.8.3;
 //"SPDX-License-Identifier: UNLICENSED"
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ACHouseToken721 is ERC721 {
+
+    address parentAddress;
+    
+    constructor() ERC721("","") {}
+
+   function setParentAddress(address _address) public {
+       parentAddress = _address;
+   }
+
+
+}
