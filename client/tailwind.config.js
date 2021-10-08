6946bdea6ff9e7b4ab389135890797e0d56de376
Adam Simms <adamsimms@gmail.com>
2021-10-08 08:44:03 -0400

    Updated branding colors.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,16 +12,16 @@ module.exports = {
       themes: [
       {
         'heartdrops': {                          /* your theme name */
-           'primary' : '#a991f7',           /* Primary color */
-           'primary-focus' : '#8462f4',     /* Primary color - focused */
+           'primary' : '#5200AD',           /* Primary color */
+           'primary-focus' : '#4E00A5',     /* Primary color - focused */
            'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
 
-           'secondary' : '#f6d860',         /* Secondary color */
-           'secondary-focus' : '#f3cc30',   /* Secondary color - focused */
-           'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
+           'secondary' : '#E800F6',         /* Secondary color */
+           'secondary-focus' : '#C900D5',   /* Secondary color - focused */
+           'secondary-content' : '#003F50', /* Foreground content color to use on secondary color */
 
-           'accent' : '#37cdbe',            /* Accent color */
-           'accent-focus' : '#2aa79b',      /* Accent color - focused */
+           'accent' : '#01C6FA',            /* Accent color */
+           'accent-focus' : '#03A6D2',      /* Accent color - focused */
            'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
 
            'neutral' : '#3d4451',           /* Neutral color */
@@ -33,10 +33,10 @@ module.exports = {
            'base-300' : '#d1d5db',          /* Base color, even more darker */
            'base-content' : '#1f2937',      /* Foreground content color to use on base color */
 
-           'info' : '#2094f3',              /* Info */
-           'success' : '#009485',           /* Success */
+           'info' : '#01C6FA',              /* Info */
+           'success' : '#FE0000',           /* Success */
            'warning' : '#ff9900',           /* Warning */
-           'error' : '#ff5724',             /* Error */
+           'error' : '#ff9900',             /* Error */
         },
       },
     {
